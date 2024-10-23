2b86be03c713a49b6339f7321517234c06702687
ilyshka <ilya_moslov@mail.ru>
2024-10-24 01:57:32 +0300

    SAVE

--- hello_world/run.sh
+++ hello_world/run.sh
@@ -1,1 +1,8 @@
+
+export TIME_ZONE=$TZ
+export TS3SERVER_DB_HOST="$(bashio::config 'db.host')"
+export TS3SERVER_DB_NAME="$(bashio::config 'db.database')"
+export TS3SERVER_DB_USER="$(bashio::config 'db.user')"
+export TS3SERVER_DB_PASSWORD="$(bashio::config 'db.password')"
+
 ./init
