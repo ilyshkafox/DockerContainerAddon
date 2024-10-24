e01d57c2979cf1cb4a1652619dd13e79a4eb8284
ilyshka <ilya_moslov@mail.ru>
2024-10-25 00:01:13 +0300

    SAVE

--- teamspeak3/run.sh
+++ teamspeak3/run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-cat /data/options.json
-export TIME_ZONE=$TZ
-export TS3SERVER_DB_HOST="$(bashio::config 'db.host')"
-export TS3SERVER_DB_NAME="$(bashio::config 'db.database')"
-export TS3SERVER_DB_USER="$(bashio::config 'db.user')"
-export TS3SERVER_DB_PASSWORD="$(bashio::config 'db.password')"
+#!/bin/bash
 
+# Чтение JSON файла и установка переменных
+TIME_ZONE=$(jq -r '.time_zone' /data/options.json)
+TS3SERVER_DB_HOST=$(jq -r '.db.host' /data/options.json)
+TS3SERVER_DB_NAME=$(jq -r '.db.database' /data/options.json)
+TS3SERVER_DB_USER=$(jq -r '.db.user' /data/options.json)
+TS3SERVER_DB_PASSWORD=$(jq -r '.db.password' /data/options.json)
+
+# Экспорт переменных окружения
+export TIME_ZONE
+export TS3SERVER_DB_HOST
+export TS3SERVER_DB_NAME
+export TS3SERVER_DB_USER
+export TS3SERVER_DB_PASSWORD
+
+# Вывод переменных для проверки
+echo "TIME_ZONE=$TIME_ZONE"
+echo "TS3SERVER_DB_HOST=$TS3SERVER_DB_HOST"
+echo "TS3SERVER_DB_NAME=$TS3SERVER_DB_NAME"
+echo "TS3SERVER_DB_USER=$TS3SERVER_DB_USER"
+echo "TS3SERVER_DB_PASSWORD=$TS3SERVER_DB_PASSWORD"
 
 /init
