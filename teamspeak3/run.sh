02b2d5cf8709a6a4956b4dc005eb242d3d724e34
ilyshka <ilya_moslov@mail.ru>
2024-10-24 23:08:40 +0300

    SAVE

--- teamspeak3/run.sh
+++ teamspeak3/run.sh
@@ -1,12 +1,3 @@
-echo "Starting..."
+#!/usr/bin/with-contenv bashio
 
-CONFIG_PATH=/data/options.json
-
-
-export TIME_ZONE=$TZ
-export TS3SERVER_DB_HOST="$(bashio::config 'db.host')"
-export TS3SERVER_DB_NAME="$(bashio::config 'db.database')"
-export TS3SERVER_DB_USER="$(bashio::config 'db.user')"
-export TS3SERVER_DB_PASSWORD="$(bashio::config 'db.password')"
-
-./init
+echo "Hello world!"
