bca173d10a40d8017208ded58a0dae71b6858058
ilyshka <ilya_moslov@mail.ru>
2024-10-25 00:02:41 +0300

    SAVE

--- teamspeak3/run.sh
+++ teamspeak3/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # Чтение JSON файла и установка переменных
 TIME_ZONE=$(jq -r '.time_zone' /data/options.json)
 TS3SERVER_DB_HOST=$(jq -r '.db.host' /data/options.json)
@@ -15,12 +13,5 @@ export TS3SERVER_DB_HOST
 export TS3SERVER_DB_NAME
 export TS3SERVER_DB_USER
 export TS3SERVER_DB_PASSWORD
-
-# Вывод переменных для проверки
-echo "TIME_ZONE=$TIME_ZONE"
-echo "TS3SERVER_DB_HOST=$TS3SERVER_DB_HOST"
-echo "TS3SERVER_DB_NAME=$TS3SERVER_DB_NAME"
-echo "TS3SERVER_DB_USER=$TS3SERVER_DB_USER"
-echo "TS3SERVER_DB_PASSWORD=$TS3SERVER_DB_PASSWORD"
 
 /init
