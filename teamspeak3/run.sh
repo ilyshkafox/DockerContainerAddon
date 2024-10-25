a89a46fb9f79b4aa0ffc1ea5c1534fbbf68484fa
ilyshka <ilya_moslov@mail.ru>
2024-10-25 03:05:21 +0300

    SAVE

--- teamspeak3/run.sh
+++ teamspeak3/run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 # Чтение JSON файла и установка переменных
-DATA_PATH=$(jq -r '.data_path' /data/options.json)
 TIME_ZONE=$(jq -r '.time_zone' /data/options.json)
 TS3SERVER_DB_HOST=$(jq -r '.db.host' /data/options.json)
 TS3SERVER_DB_NAME=$(jq -r '.db.database' /data/options.json)
 TS3SERVER_DB_USER=$(jq -r '.db.user' /data/options.json)
 TS3SERVER_DB_PASSWORD=$(jq -r '.db.password' /data/options.json)
-
-
 TS3SERVER_DB_PLUGIN=ts3db_mariadb
 TS3SERVER_DB_SQLCREATEPATH=create_mariadb
 TS3SERVER_DB_WAITUNTILREADY=30
 TS3SERVER_LICENSE=accept
 
 # Экспорт переменных окружения
-export DATA_PATH
 export TIME_ZONE
 export TS3SERVER_DB_HOST
 export TS3SERVER_DB_NAME
@@ -26,10 +22,5 @@ export TS3SERVER_DB_SQLCREATEPATH
 export TS3SERVER_DB_WAITUNTILREADY
 export TS3SERVER_LICENSE
 
-
-mkdir -p "$DATA_PATH"
-if [ -d "/teamspeak/save" ]; then rm -Rf "/teamspeak/save"; fi
-ln -fs "$DATA_PATH" "/teamspeak/save"
-chown ts:ts -R "$DATA_PATH"
 
 /init
